861cbcda0ea00c8994470cb3c87c68316866faf4
michelle nderi <nderimichelle@gmail.com>
2023-04-10 12:25:23 +0300

    Added render cards function

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function clearRecipeCards() {
   // add loader to recipecards
   recipeCards.classList.add("loader");
 }
+
 // Event listener for search form submission
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -41,5 +42,85 @@ searchForm.addEventListener("submit", (e) => {
     .catch((error) => console.log(error));
 });
 
+//Function to add recipes to meal plan
+function AddRecipeToMealPlan(recipeId) {
+  const divId = `#recipeCard${recipeId}`;
+  // add recipe id to meal plan cards 
+  const currentRecipeCard = mealPlanCards.querySelector(divId);
+  if (currentRecipeCard) {
+    return;
+  }
+
+  let url = `https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=false&apiKey=${apiKey}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      renderRecipeCard(data, "plan");
+    })
+    .catch((error) => console.log(error));
+}
+
+//Function to render recipe cards
+function renderRecipeCard(result, type) {
+  const recipeCard = document.createElement("div");
+  recipeCard.className = "recipe-card";
+  recipeCard.id = `recipeCard${result.id}`;
+
+  recipeCard.innerHTML = `
+    <h3>${result.title}</h3>
+  `;
+  // Condition to render image if it is in the returned results
+  if (result.image) {
+    recipeCard.innerHTML =
+      recipeCard.innerHTML +
+      `  <img src="${result.image}" alt="${result.title}">`;
+  }
+  //Condition to render source url if it is in the returned results
+  if (result.sourceUrl) {
+    const recipeSourceUrl = document.createElement("a");
+    recipeSourceUrl.innerText = result.title;
+    recipeSourceUrl.href = result.sourceUrl;
+    recipeSourceUrl.target = "blank";
+    recipeCard.appendChild(recipeSourceUrl);
+  }
+  recipeCard.innerHTML = recipeCard.innerHTML + `</br>`;
+//Create add plan btn
+  const managePlanBtn = document.createElement("button");
+  managePlanBtn.className = "recipe-button";
+  managePlanBtn.dataset.recipeId = result.id;
+  recipeCard.appendChild(managePlanBtn);
+  //Add similar recipes
+  const similarRecipesBtn = document.createElement("button");
+  similarRecipesBtn.textContent = "Similar recipes";
+  similarRecipesBtn.dataset.recipeId = result.id;
+  similarRecipesBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getSimilarRecipes(e.target.dataset.recipeId);
+  });
+  recipeCard.appendChild(similarRecipesBtn);
+  //Conditon if the button is a child of results of search  function
+  if (type == "search") {
+    managePlanBtn.textContent = "Add to My Plan";
+    managePlanBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      AddRecipeToMealPlan(e.target.dataset.recipeId);
+    });
+    recipeCards.appendChild(recipeCard);
+  } 
+  // Conditon if the button is a child of results of add meal plan function
+  else if (type == "plan") {
+    managePlanBtn.textContent = "Remove";
+    managePlanBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const recipeId = e.target.dataset.recipeId;
+      const divId = `#recipeCard${recipeId}`;
+      const currentRecipeCard = mealPlanCards.querySelector(divId);
+      if (currentRecipeCard) {
+        mealPlanCards.removeChild(currentRecipeCard);
+      }
+    });
+    mealPlanCards.appendChild(recipeCard);
+  }
+}
 })
 
